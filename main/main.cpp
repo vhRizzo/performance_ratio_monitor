00172ec8b7449bd495be5fdb900a2462e6105f70
Victor Hugo Rizzo Moura <victor.rizzomoura@gmail.com>
2023-09-25 15:41:05 -0300

    Updated documentation from main and ads1115.

--- main/main.cpp
+++ main/main.cpp
@@ -1,4 +1,4 @@
-extern "C" { /* Converte o codigo .cpp para .c */
+extern "C" {  // Converte o código .cpp para .c
 
 #include "Modem_SmartModular.hpp"
 #include "global_data.h"
@@ -23,7 +23,7 @@ void app_main(void);
 }
 
 void app_main(void) {
-    /* Verifica por problemas na memória flash do ESP */
+    // Verifica por problemas na memória flash do ESP
     esp_err_t ret = nvs_flash_init();
     if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
         ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
@@ -39,49 +39,42 @@ void app_main(void) {
     gpio_set_level(GPIO_NUM_2, 0);  // Apaga o LED (define o sinal para baixo)
     vTaskDelay(1000 / portTICK_PERIOD_MS);  // Aguarda 1 segundo
     gpio_set_level(GPIO_NUM_2, 1);  // Acende o LED (define o sinal para alto)
-    /* O LED serve para indicar que o ESP esta funcionando corretamente, O LED
-     * permanecera aceso, e se piscar e sinal de que o ESP reiniciou */
+    /* A função do LED é ser um controle de funcionamento. O LED deverá
+     * permanecer aceso durante todo o funcionamento do dispositivo, se o LED
+     * estiver desligado ou piscando, é sinal de que há problemas no sistema. */
 
-    esp_wifi_stop();  // Interrompe o uso do WiFi (não será utilizado neste
-                      // projeto)
+    // Interrompe o uso do WiFi (não será utilizado neste projeto)
+    esp_wifi_stop();
 
-    // ***** Fazer apenas uma vez!!!! (Alterar as configurações da antena LoRa)
+    /* O trecho de código comentado abaixo deve ser executado apenas quando for
+     * necessário utilizar um novo módulo LoRa para configurá-lo e validá-lo.
+     *
+     * @warning EXECUTAR ESTE TRECHO DE CÓDIGO APENAS UMA VEZ PARA CONFIGURAR UM
+     * NOVO MÓDULO, APÓS CONFIGURADO, COMENTE O CÓDIGO NOVAMENTE. */
     // Modem_SmartModular teste(UART_NUM_2);
     // teste.OTAA_APPEUI("00:00:00:00:00:00:00:00");
     // teste.OTAA_APPKEY("8C:17:01:D4:15:AB:D5:FB:09:58:CB:35:3E:F6:60:C1");
     // teste.salvar_config();
-    // ***** As configs ficarão salvas para o auto_join ser ativado!
 
 #if defined(BME280_SENSOR) || defined(ADS1115_SENSOR)
-    ESP_ERROR_CHECK(i2c_init());  // se algum dos sensores I2C estiverem sendo
-                                  // usados, initializa o I2C
+    // Se algum dos sensores I2C estiverem sendo usados, initializa o I2C
+    ESP_ERROR_CHECK(i2c_init());
 #endif
 
 #ifdef ADS1115_SENSOR
     // Cria uma fila para enviar itens de uma tarefa para outra
     ads_queue = xQueueCreate(1, sizeof(ads_t));
-    /*
-     * Cria a tarefa do conversor analógico-digital.
-     * Tarefas sao funções que geralmente se mantem em um loop infinito para
-     * realizar determinadas tarefas. Semelhante ao loop do Arduino, com o
-     * diferencial de que varias tarefas podem ser executadas simultaneamente.
-     */
-    xTaskCreatePinnedToCore(
-        ads1115_task,    // Função da tarefa
-        "ADS1115_Task",  // Nome para controle de depuração
-        4 * 1024,  // Tamanho da pilha de execução que será alocado para a
-                   // tarefa
-        NULL,      // Parâmetros da tarefa (geralmente não utilizado)
-        configMAX_PRIORITIES -
-            3,  // Prioridade da tarefa (maior valor terá maior prioridade)
-        NULL,  // Handler para esta tarefa caso seja necessário que outra tarefa
-               // ou função se comunique/controle esta
-        1);  // Nucleo do ESP que será utilizado para executar a tarefa
+    /* Cria a tarefa do conversor analógico-digital.
+     *
+     * Tarefas são funções que geralmente se mantém em um loop infinito para
+     * realizar determinados procedimentos, semelhante ao loop do Arduino, porém
+     * possui a capacidade de executar várias tarefas simultaneamente. */
+    xTaskCreatePinnedToCore(ads1115_task, "ADS1115_Task", 4 * 1024, NULL,
+                            configMAX_PRIORITIES - 3, NULL, 1);
 #endif
 
     /* Daqui em diante apenas repete o mesmo processo acima para os demais
-     * sensores ou protocolos necessários */
-
+     * sensores ou protocolos necessários. */
 #ifdef DS18B20_SENSOR
     ds18b20_queue = xQueueCreate(1, sizeof(ds18b20_t));
     xTaskCreatePinnedToCore(ds18b20_task, "DS18B20_Task", 4 * 1024, NULL,
@@ -105,14 +98,12 @@ void app_main(void) {
     xTaskCreatePinnedToCore(neo6m_task, "NEO-6M_Task", 4 * 1024, NULL,
                             configMAX_PRIORITIES - 7, NULL, 0);
 #endif
-    /*
-     * As tarefas abaixo são de leitura e envio de dados, estas tarefas usarão
+    /* As tarefas abaixo são de leitura e envio de dados, estas tarefas usarão
      * as filas geradas acima para receber os itens para então enviá-los via
      * LoRaWAN, portanto é feito um atraso aqui para garantir que as filas já
      * estejam todas operantes, pois caso estas tarefas tentem ler um item de
      * uma fila que ainda não existe, um erro fatal será gerado, reiniciando o
-     * ESP.
-     */
+     * ESP. */
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     xTaskCreatePinnedToCore(rcv_data_task, "Data_Receive_Task", 4 * 1024, NULL,
                             configMAX_PRIORITIES - 2, NULL, 1);
