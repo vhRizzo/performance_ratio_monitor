41b1ee4ab387c9c51524a7bd14aab215daf10147
Victor Hugo Rizzo Moura <victor.rizzomoura@gmail.com>
2023-09-26 10:54:24 -0300

    Changed indentation on Modem_SmartModular and neo6m_setup.

--- main/components/include/neo6m_setup.h
+++ main/components/include/neo6m_setup.h
@@ -3,9 +3,9 @@
 
 #include "global_data.h"
 
-#define RD_BUF_SIZE     1024
+#define RD_BUF_SIZE 1024
 #define PATTERN_CHR_NUM (1)
 
-void neo6m_task ( void *pvParameters );
+void neo6m_task(void *pvParameters);
 
 #endif /* NEO6M_SETUP_H */
